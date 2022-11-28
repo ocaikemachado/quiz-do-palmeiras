b6e79bd4bb61e4a1d944af5fde5a74299cd43aa0
ocaikemachado <112231891+ocaikemachado@users.noreply.github.com>
2022-11-28 19:33:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let perguntas = [
 {
-    titulo: 'Ano de fundacao',
+    titulo: 'Ano de fundação',
     alternativas: ['1914', '1904', '1901', '1912'],
     correta: 0
 },
@@ -13,6 +13,41 @@ let perguntas = [
     titulo: 'Jogador que mais atuou pelo clube',
     alternativas: ['Dudu', 'Waldemar Fiume', 'Leao', 'Ademir da Guia'],
     correta: 3
+},
+{
+    titulo: 'Compositor do hino',
+    alternativas: ['Antônio Sergi', 'Lauro D’Avila', 'Henrique Paganetto Roma', 'Cláudio Ribeiro'],
+    correta: 0
+},
+{
+    titulo: 'Apelido do estádio',
+    alternativas: ['Maracanã', 'Pacaembu', 'Allianz Arena', 'Arena Palestra Itália'],
+    correta: 3
+},
+{
+    titulo: 'Primeiro nome do clube',
+    alternativas: ['Palestra', 'Palmeiras', 'Società Sportiva Palestra Italia', 'União Palmeiras'],
+    correta: 2
+},
+{
+    titulo: 'Datas em que foi campeão da libertadores',
+    alternativas: ['1999, 2021 e 2022', '2000, 2001 e 2022', '2020, 2021, 2022', '2002, 2004, 2020 e 2022'],
+    correta: 0
+},
+{
+    titulo: 'Técnico que mais ganhou títulos pelo clube',
+    alternativas: ['Adriano Chuva', 'Oswaldo Brandão', 'Abel Braga', 'Vanderlei Luxemburgo'],
+    correta: 3
+},
+{
+    titulo: 'Capitão no título da libertadores 2021',
+    alternativas: ['Gustavo Gómez', 'Gustavo Scarpa', 'Dudu', 'Murilo'],
+    correta: 0
+},
+{
+    titulo: 'Gol da vitória na final da libertadores de 2021',
+    alternativas: ['Andreas Pereira', 'Rony', 'Deyverson', 'Raphael Veiga'],
+    correta: 2
 }
 ]
 
@@ -53,9 +88,11 @@ checaResposta: function(user){
     if(this.qatual.correta == user){
         console.log("Correta!")
         this.TotalPontos++;
+        // this.mostraresposta();
     }
     else{
         console.log("Errada!")
+         this.mostraresposta()
     }
     this.atualizaPontos();
     this.Proximaperg();
@@ -64,10 +101,28 @@ checaResposta: function(user){
 
 atualizaPontos: function(){
     let scoreDiv = document.getElementById('pontos');
-    scoreDiv.textContent = `Sua pontuacao: ${this.TotalPontos}`;
+    scoreDiv.textContent = `Sua pontuação: ${this.TotalPontos}`;
+},
+
+mostraresposta: function(correto){
+    let resultDiv = document.getElementById('result');
+    let result = ''
+    // formatar como mensagem será exibida
+    if(correto == true){
+        result = 'Resposta Correta';
+    }
+    else{
+        // obtendo questão atual
+        let pergunta = perguntas[this.Atualpos];
+        // obtenha o indice da resposta correta da questão atual
+        let cindice = pergunta.correta;
+        // obtenha o texto da resposta correta da questão atual
+        let ctexto = pergunta.alternativas[cindice];
+        result = `Incorreto! Resposta Correta: ${ctexto}`;
+    }
+    resultDiv.textContent = result;
+
 }
-
-
 }
 
 app.start();
